e53ba96a156f99b93d852c1d22edc057fb6d1858
patioglass <patiopatimon@gmail.com>
2020-12-24 23:24:20 +0900

    いっぱいだせるようにした

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 window.onload = () => {
-    const christmasTree = document.getElementById("christmasTree");
-    const footerContents = document.getElementById("footerContents");
-    const replayButton = document.getElementById("replay");
+    const christmasTree = document.querySelectorAll(".christmasTree");
     const tweetButton = document.getElementById("tweet");
+    const loopElement = document.getElementById("loop");
     let fallDownCount = 0;
 
-    christmasTree.addEventListener("animationend", () => {
-        footerContents.style.display = "block";
-        tweetButton.href = `http://twitter.com/share?text=そのためのクリスマスツリー - ${fallDownCount}回倒した&url=${location.href}&hashtags=そのためのクリスマスツリー`;      
+    Array.from(christmasTree).forEach((target) => {
+        const w = screen.width;
+        const settingWidth =  Math.floor(Math.random() * Math.floor(w - 1));
+
+        target.style.left = `${settingWidth}px`;
+        addListenerImg(target);
     })
-    christmasTree.addEventListener("click", () => {
-        christmasTree.classList.add("fallDown");
-        fallDownCount++;
-    })
-    replayButton.addEventListener("click", () => {
-        footerContents.style.display = "none";
-        christmasTree.classList.remove("fallDown");
-    })
+    
+    const createTree = setInterval(() => {
+        const tree = document.querySelectorAll(".christmasTree");
+        const w = screen.width;
+        for(let i = 0; i < 8 - tree.length; i++) {
+            const settingWidth =  Math.floor(Math.random() * Math.floor(w - 1));
+            const newTree =  document.createElement("img");
+            newTree.src = "christmastree.png";
+            newTree.classList.add("christmasTree");
+            newTree.style.left = `${settingWidth}px`;
+            addListenerImg(newTree);
+            loopElement.appendChild(newTree);
+        }
+    }, 1000)
+    function addListenerImg(element) {
+        element.addEventListener("animationend", () => {
+            element.classList.remove("christmasTree");
+    
+            if (fallDownCount > 1) {
+                tweetButton.href = `http://twitter.com/share?text=そのためのクリスマスツリー - ${fallDownCount}回倒した&url=${location.href}&hashtags=そのためのクリスマスツリー`;
+            } else {
+                tweetButton.href = `http://twitter.com/share?text=そのためのクリスマスツリー&url=${location.href}&hashtags=そのためのクリスマスツリー`;
+            }
+        })
+        element.addEventListener("click", () => {
+            element.classList.add("fallDown");
+            fallDownCount++;
+        })
+    }
 }
